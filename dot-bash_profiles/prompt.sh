72f78040574be5aaa3cbb8f3d4eec9edcc3cdcbc
Michael Schubert <michael@schubert.cx>
2009-11-13 22:46:41 +0000

    leftover cruft

--- dot-bash_profiles/prompt.sh
+++ dot-bash_profiles/prompt.sh
@@ -20,6 +20,5 @@ GIT_PS1_SHOWDIRTYSTATE=false
 GIT_PS1_SHOWSTASHSTATE=true
 GIT_PS1_SHOWUNTRACKEDFILES=true
 
-echo $(git_branch_dirty)
 PS1="$PREFIX$USER_AT_HOST$BASE\$(git_prefix)\$(git_branch_dirty_check -eq 0 && printf \"${RED}\" || printf \"${GREEN}\")\$(git_branch)\$(git_branch_untracked_check -eq 0 && printf \"${CYAN}\")\$(git_branch_untracked)\$(git_branch_stashed_check -eq 0 && printf \"${YELLOW}\")\$(git_branch_stashed)$SUFFIX"
 export PS1
