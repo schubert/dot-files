144370635e98dee4f68a1bdc6be079cb5e8f8398
Michael Schubert <michael@schubert.cx>
2010-07-08 01:37:21 +0000

    add some nice functions and aliases lifted from
    http://superuser.com/questions/52483/terminal-tips-and-tricks-for-mac-os-x

--- dot-bash_profiles/aliases.sh
+++ dot-bash_profiles/aliases.sh
@@ -17,3 +17,31 @@ alias pull='git pull'
 alias push='git push'
 alias rc='rake commit'
 alias mergemaster='git merge --no-commit --summary origin master'
+alias ql='qlmanage -p 2>/dev/null'
+alias memhog='top -o vsize'
+alias cpuhog='top -o cpu'
+
+alias maze='emacs -batch -l dunnet'
+
+pman () {
+    man -t "${1}" | open -f -a /Applications/Preview.app
+}
+
+tman () {
+  MANWIDTH=160 MANPAGER='col -bx' man $@ | mate
+}
+
+# Quit an OS X application from the command line
+quit () {
+    for app in $*; do
+        osascript -e 'quit app "'$app'"'
+    done
+}
+
+relaunch () {
+    for app in $*; do
+        osascript -e 'quit app "'$app'"';
+        sleep 2;
+        open -a $app
+    done
+}
